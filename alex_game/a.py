cde1478b918e1ddaa8d6b740a95e4c63a34169ea
Artem Glukhov <artglukhov@gmail.com>
2018-09-24 19:32:38 +0300

    Third lesson

--- alex_game/a.py
+++ alex_game/a.py
@@ -5,7 +5,7 @@ from settings import lazurit
 from settings import transparent
 from settings import size, white
 
-
+side = 'right'
 pygame.init()
 pygame.display.set_caption('ny sho govno, opyat\' nazralsya?')
 screen = pygame.display.set_mode(size)
@@ -19,36 +19,70 @@ player_y = 57 * 2
 spritesheet = pygame.transform.scale2x(pygame.image.load('animation.gif'))
 player = spritesheet.subsurface(pygame.Rect(player_x * 1,  player_y, player_x, player_y))
 
-frames_jump = []
-frames_jump.append(spritesheet.subsurface(pygame.Rect( player_x * 1,  player_y * 2, player_x, player_y)))
-frames_jump.append(spritesheet.subsurface(pygame.Rect( player_x * 2,  player_y * 2, player_x, player_y)))
-frames_jump.append(spritesheet.subsurface(pygame.Rect( player_x * 3,  player_y * 2, player_x, player_y)))
-frames_jump.append(spritesheet.subsurface(pygame.Rect( player_x * 4,  player_y * 2, player_x, player_y)))
-frames_jump.append(spritesheet.subsurface(pygame.Rect( player_x * 5,  player_y * 2, player_x, player_y)))
-frames_jump.append(spritesheet.subsurface(pygame.Rect( player_x * 6,  player_y * 2, player_x, player_y)))
-frames_jump.append(spritesheet.subsurface(pygame.Rect( player_x * 0,  player_y * 3, player_x, player_y)))
-frames_jump.append(spritesheet.subsurface(pygame.Rect( player_x * 1,  player_y * 3, player_x, player_y)))
-frames_jump.append(spritesheet.subsurface(pygame.Rect( player_x * 2,  player_y * 3, player_x, player_y)))
+frames_jump_right = []
+frames_jump_right.append(spritesheet.subsurface(pygame.Rect( player_x * 1,  player_y * 2, player_x, player_y)))
+frames_jump_right.append(spritesheet.subsurface(pygame.Rect( player_x * 2,  player_y * 2, player_x, player_y)))
+frames_jump_right.append(spritesheet.subsurface(pygame.Rect( player_x * 3,  player_y * 2, player_x, player_y)))
+frames_jump_right.append(spritesheet.subsurface(pygame.Rect( player_x * 4,  player_y * 2, player_x, player_y)))
+frames_jump_right.append(spritesheet.subsurface(pygame.Rect( player_x * 5,  player_y * 2, player_x, player_y)))
+frames_jump_right.append(spritesheet.subsurface(pygame.Rect( player_x * 6,  player_y * 2, player_x, player_y)))
+frames_jump_right.append(spritesheet.subsurface(pygame.Rect( player_x * 0,  player_y * 3, player_x, player_y)))
+frames_jump_right.append(spritesheet.subsurface(pygame.Rect( player_x * 1,  player_y * 3, player_x, player_y)))
+frames_jump_right.append(spritesheet.subsurface(pygame.Rect( player_x * 2,  player_y * 3, player_x, player_y)))
 
-frames_stay = []
-frames_stay.append(spritesheet.subsurface(pygame.Rect(player_x * 0, player_y * 0, player_x, player_y)))
-frames_stay.append(spritesheet.subsurface(pygame.Rect(player_x * 1, player_y * 0, player_x, player_y)))
-frames_stay.append(spritesheet.subsurface(pygame.Rect(player_x * 2, player_y * 0, player_x, player_y)))
-frames_stay.append(spritesheet.subsurface(pygame.Rect(player_x * 3, player_y * 0, player_x, player_y)))
+frames_jump_left = []
+frames_jump_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 1,  player_y * 2, player_x, player_y)), True, False))
+frames_jump_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 2,  player_y * 2, player_x, player_y)), True, False))
+frames_jump_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 3,  player_y * 2, player_x, player_y)), True, False))
+frames_jump_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 4,  player_y * 2, player_x, player_y)), True, False))
+frames_jump_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 5,  player_y * 2, player_x, player_y)), True, False))
+frames_jump_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 6,  player_y * 2, player_x, player_y)), True, False))
+frames_jump_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 0,  player_y * 3, player_x, player_y)), True, False))
+frames_jump_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 1,  player_y * 3, player_x, player_y)), True, False))
+frames_jump_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 2,  player_y * 3, player_x, player_y)), True, False))
 
-frames_run = []
-frames_run.append(spritesheet.subsurface(pygame.Rect( player_x * 1,  player_y, player_x, player_y)))
-frames_run.append(spritesheet.subsurface(pygame.Rect( player_x * 2,  player_y, player_x, player_y)))
-frames_run.append(spritesheet.subsurface(pygame.Rect( player_x * 3,  player_y, player_x, player_y)))
-frames_run.append(spritesheet.subsurface(pygame.Rect( player_x * 4,  player_y, player_x, player_y)))
-frames_run.append(spritesheet.subsurface(pygame.Rect( player_x * 5,  player_y, player_x, player_y)))
-frames_run.append(spritesheet.subsurface(pygame.Rect( player_x * 6,  player_y, player_x, player_y)))
+frames_stay_right = []
+frames_stay_right.append(spritesheet.subsurface(pygame.Rect(player_x * 0, player_y * 0, player_x, player_y)))
+frames_stay_right.append(spritesheet.subsurface(pygame.Rect(player_x * 1, player_y * 0, player_x, player_y)))
+frames_stay_right.append(spritesheet.subsurface(pygame.Rect(player_x * 2, player_y * 0, player_x, player_y)))
+frames_stay_right.append(spritesheet.subsurface(pygame.Rect(player_x * 3, player_y * 0, player_x, player_y)))
 
-for f in frames_jump:
+frames_stay_left = []
+frames_stay_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect(player_x * 0, player_y * 0, player_x, player_y)), True, False))
+frames_stay_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect(player_x * 1, player_y * 0, player_x, player_y)), True, False))
+frames_stay_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect(player_x * 2, player_y * 0, player_x, player_y)), True, False))
+frames_stay_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect(player_x * 3, player_y * 0, player_x, player_y)), True, False))
+
+
+
+
+frames_run_right = []
+frames_run_right.append(spritesheet.subsurface(pygame.Rect( player_x * 1,  player_y, player_x, player_y)))
+frames_run_right.append(spritesheet.subsurface(pygame.Rect( player_x * 2,  player_y, player_x, player_y)))
+frames_run_right.append(spritesheet.subsurface(pygame.Rect( player_x * 3,  player_y, player_x, player_y)))
+frames_run_right.append(spritesheet.subsurface(pygame.Rect( player_x * 4,  player_y, player_x, player_y)))
+frames_run_right.append(spritesheet.subsurface(pygame.Rect( player_x * 5,  player_y, player_x, player_y)))
+frames_run_right.append(spritesheet.subsurface(pygame.Rect( player_x * 6,  player_y, player_x, player_y)))
+
+frames_run_left = []
+frames_run_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 1,  player_y, player_x, player_y)), True, False))
+frames_run_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 2,  player_y, player_x, player_y)), True, False))
+frames_run_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 3,  player_y, player_x, player_y)), True, False))
+frames_run_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 4,  player_y, player_x, player_y)), True, False))
+frames_run_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 5,  player_y, player_x, player_y)), True, False))
+frames_run_left.append(pygame.transform.flip(spritesheet.subsurface(pygame.Rect( player_x * 6,  player_y, player_x, player_y)), True, False))
+
+for f in frames_jump_right:
     f.set_colorkey(f.get_at((0,0)))
-for f in frames_run:
+for f in frames_jump_left:
     f.set_colorkey(f.get_at((0,0)))
-for f in frames_stay:
+for f in frames_run_left:
+    f.set_colorkey(f.get_at((0,0)))
+for f in frames_stay_left:
+    f.set_colorkey(f.get_at((0,0)))
+for f in frames_run_right:
+    f.set_colorkey(f.get_at((0,0)))
+for f in frames_stay_right:
     f.set_colorkey(f.get_at((0,0)))
 
 moving = False
@@ -56,7 +90,7 @@ i = 0
 z = (0, 440)
 base_x = (0)
 base_y =(440)
-frames = frames_stay
+frames = frames_stay_right
 jumping = 0
 jump_y = [0,16,32,48,64,48,32,16,0]
 pygame.font.init()
@@ -73,21 +107,29 @@ while True:
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
             moving = True
-            frames = frames_run
+            frames = frames_run_right
             distance = 20
+            side = 'right'
         if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
             moving = True
-            frames = frames_run
+            frames = frames_run_left
             distance = -20
+            side = 'left'
         if event.type == pygame.KEYUP and event.key == pygame.K_a:
             moving = False
-            frames = frames_stay
+            frames = frames_stay_left
+            side  = 'left'
         if event.type == pygame.KEYUP and event.key == pygame.K_d:
             moving = False
-            frames = frames_stay
+            frames = frames_stay_right
+            side  = 'right'
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             jumping = True
-            frames = frames_jump
+            if side  == 'right':
+                frames = frames_jump_right
+            else:
+                frames = frames_jump_left
+
         if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
             z = (z[0], 440)
             moving = False
@@ -117,7 +159,10 @@ while True:
     if i >= len(frames):
         if jumping:
             jumping = False
-            frames = frames_stay
+            if side == 'right':
+                frames = frames_run_right
+            else:
+                frames = frames_run_left
         i = 0
 
     pygame.mouse.set_visible(False)
