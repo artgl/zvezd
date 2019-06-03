8f6283ce16c99d46c6e82bb8fb32e9c186ce610f
Artem Glukhov <artglukhov@gmail.com>
2019-06-03 20:43:17 +0300

    New local runner

---
+++ tanks/local-runner-new.sh
@@ -0,0 +1,5 @@
+while [[ `xdotool search Run | xargs -I{} xdotool getwindowgeometry {} | grep "960" | wc -l` != 3 ]]; do
+  killall -9 java;
+  nohup java -cp ".:local-runner-new.jar" Run -base-adapter-port=31000 -debug=true -tick-count=5000 -render-to-screen-scale=0.75 -render-to-screen=true -render-to-screen-sync=true -bunker=false -p1-name=first -p2-name=second -results-file=result.txt ""#LocalTestPlayer"" ""#LocalTestPlayer"" &
+  sleep 1
+done
