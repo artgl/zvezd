c106c51209e781ef60d85c06fb4d6e71430a17f7
Artem Gluhov <artem_gluhov@relex.ru>
2019-12-16 02:26:00 +0300

    My strategy  for AI cup codeside 2019

---
+++ ai_cup_2019/my_strategy.py
@@ -0,0 +1,67 @@
+import model
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+import copy
+import pprint
+import random
+import pathfinding
+from one_step_to import one_step_to
+
+pathfinding.finder.MAX_RUNS = 10
+
+def distance_sqr(a, b):
+            return (a.x - b.x) ** 2 + (a.y - b.y) ** 2
+
+class MyStrategy:
+    def __init__(self):
+        pass
+
+    def get_action(self, unit, game, debug):
+        # Replace this code with your own
+
+        nearest_enemy = min(
+            filter(lambda u: u.player_id != unit.player_id, game.units),
+            key=lambda u: distance_sqr(u.position, unit.position),
+            default=None)
+        nearest_weapon = min(
+            filter(lambda box: isinstance(
+                box.item, model.Item.Weapon) and box.item.weapon_type == 0, game.loot_boxes),
+            key=lambda box: distance_sqr(box.position, unit.position),
+            default=None)
+       
+        target_pos = unit.position
+        if (unit.weapon is None or (unit.weapon.typ != model.WeaponType.PISTOL)) and nearest_weapon is not None:
+            target_pos = nearest_weapon.position
+        elif nearest_enemy is not None:
+            target_pos = nearest_enemy.position
+
+        pos = one_step_to(game, unit, target_pos)
+        if pos:
+            target_pos = pos
+
+        debug.draw(model.CustomData.Log("Target pos: {}".format(target_pos)))
+        debug.draw(model.CustomData.Log("Unit pos: {}".format(unit.position)))
+        aim = model.Vec2Double(0, 0)
+        if nearest_enemy is not None:
+            aim = model.Vec2Double(
+                nearest_enemy.position.x - unit.position.x,
+                nearest_enemy.position.y - unit.position.y)
+        jump = target_pos.y > unit.position.y \
+                or game.level.tiles[int(unit.position.x)][int(unit.position.y - 1)] == model.Tile.EMPTY
+        if target_pos.x > unit.position.x and game.level.tiles[int(unit.position.x + 1)][int(unit.position.y)] == model.Tile.WALL:
+            jump = True
+        if target_pos.x < unit.position.x and game.level.tiles[int(unit.position.x - 1)][int(unit.position.y)] == model.Tile.WALL:
+            jump = True
+
+           
+        return model.UnitAction(
+            velocity=10 * (target_pos.x - unit.position.x),
+#            velocity=100 if target_pos.x > unit.position.x else -100,
+            jump=jump,
+            jump_down=not jump,
+            aim=aim,
+            shoot=True,
+            reload=False,
+            swap_weapon=(unit.weapon and (unit.weapon.typ != model.WeaponType.PISTOL)),
+            plant_mine=False)
---
+++ ai_cup_2019/one_step_to.py
@@ -0,0 +1,97 @@
+import model
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+import copy
+import pprint
+import random
+import pathfinding
+
+
+def distance_sqr(a, b):
+     return (a.x - b.x) ** 2 + (a.y - b.y) ** 2
+
+def one_step_to(game, unit, target_position):
+
+    tiles = game.level.tiles
+
+    len_x = len(game.level.tiles)
+    len_y = len(game.level.tiles[0])
+
+    my_map = copy.deepcopy(tiles)
+
+    for x in range(len_x):
+        for y in reversed(range(len_y)):
+            my_map[x][y] = 0
+            if tiles[x][y] == model.Tile.LADDER or tiles[x][y] == model.Tile.PLATFORM:
+                my_map[x][y] = 3
+                if tiles[x][y+1] == model.Tile.EMPTY:
+                    my_map[x][y+1] = 3
+            if tiles[x][y] == model.Tile.EMPTY and tiles[x][y - 1] == model.Tile.WALL:
+                my_map[x][y] = 3
+            if tiles[x][y] == model.Tile.EMPTY and tiles[x][y - 2] == model.Tile.WALL:
+                my_map[x][y] = 9
+            if tiles[x][y] == model.Tile.EMPTY and tiles[x][y - 3] == model.Tile.WALL:
+                my_map[x][y] = 9
+
+    for x in range(len_x):
+        for y in reversed(range(len_y)):
+            if my_map[x][y] == 3:
+                if tiles[x-1][y] == model.Tile.EMPTY:
+                    my_map[x-1][y] = 9
+#                if x > 1 and tiles[x-2][y] == model.Tile.EMPTY:
+#                    my_map[x-2][y] = 3
+
+
+    for x in reversed(range(len_x)):
+        for y in reversed(range(len_y)):
+            if my_map[x][y] == 3:
+                if tiles[x+1][y] == model.Tile.EMPTY:
+                    my_map[x+1][y] = 9
+#                if x < (len_x - 2) and tiles[x+2][y] == model.Tile.EMPTY:
+#                    my_map[x+2][y] = 3
+
+    # fall down
+    for y in reversed(range(len_y)):
+        for x in range(len_x):
+            if my_map[x][y] > 0:
+                y1 = y - 1
+                while y1 >= 0 and tiles[x][y1] == model.Tile.EMPTY:
+                    if my_map[x][y1] == 0:
+                        my_map[x][y1] = 9
+                    y1 = y1 - 1
+                if my_map[x-1][y-1] == 0 and tiles[x-1][y-1] == model.Tile.EMPTY:
+                    my_map[x-1][y-1] = 9
+                if my_map[x+1][y-1] == 0 and tiles[x+1][y-1] == model.Tile.EMPTY:
+                    my_map[x+1][y-1] = 9
+
+    print(unit.position.x, unit.position.y)
+
+    # print my_map
+    for y in reversed(list(range(len_y))):
+        line = []
+        for x in range(len_x):
+            line.append(my_map[x][y])
+        print(line)
+
+    # preparing matrix - we should invert y axis    
+    matrix = []
+    for y in range(len_y):
+        line = []
+        for x in range(len_x):
+            line.append(my_map[x][y])
+        matrix.insert(0, line)
+
+    grid = Grid(matrix=matrix)
+    start = grid.node(int(unit.position.x), len_y - 1 - int(unit.position.y))
+#    end = grid.node(3, len_y - 1 - 15)
+    end = grid.node(int(target_position.x), len_y - 1 - int(target_position.y))
+    finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
+    path, runs = finder.find_path(start, end, grid)
+    if len(path) > 1:
+        print(grid.grid_str(path=path, start=start, end=end))
+        return model.Vec2Float(path[1][0], len_y - 1 - path[1][1])
+    else:
+        print(grid.grid_str(start=start, end=end))
+        return None
+
